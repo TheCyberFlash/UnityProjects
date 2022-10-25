936c40d57e6e41a154bd0b0493e915cad6456aaf
Steven Jaguric <steven.jaguric@gmail.com>
2022-10-25 15:46:20 +0200

    Setup for TDS Proj

---
+++ CraftpixTDS/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.3f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void Update()
+    {
+        Vector3 goalPos = target.position;
+        goalPos.z = transform.position.z;
+        transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smoothTime);
+    }
+}
